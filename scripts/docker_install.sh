afe6f8d14082e0282988bdb727b8b226dbbb4476
m-brl <mathieu.borel@epitech.eu>
2025-03-27 13:40:46 +0100

    evol(script): evol docker install script
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -9,20 +9,7 @@ else
     exit 1
 fi
 
-OS_TYPE="$(uname)"
-if [[ "$OS_TYPE" == "Darwin" ]]; then
-    echo "macOS detected"
-    DOCKER_UID=$(id -u) DOCKER_GID=$(id -g) $DOCKER_COMPOSE -f docker-compose.yml -f docker-compose.x11.yml up -d
-elif [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-    echo "Wayland detected"
-    DOCKER_UID=$(id -u) DOCKER_GID=$(id -g) $DOCKER_COMPOSE -f docker-compose.yml -f docker-compose.wayland.yml up -d
-elif [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
-    echo "X11 detected"
-    DOCKER_UID=$(id -u) DOCKER_GID=$(id -g) $DOCKER_COMPOSE -f docker-compose.yml -f docker-compose.x11.yml up -d
-else
-    echo -e "\033[31m\033[1mInvalid session type\033[0m"
-    exit 1
-fi
+DOCKER_UID=$(id -u) DOCKER_GID=$(id -g) $DOCKER_COMPOSE -f docker-compose.yml up -d
 
 echo -e "\033[32m\033[1mInstallation complete\033[0m, you can now access the container with the following commands:"
 echo "  xhost +"
